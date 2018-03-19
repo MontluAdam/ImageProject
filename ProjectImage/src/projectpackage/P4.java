eb3e76741a4c54d6d419695cb84196e199bd8e65
Ozan Mdul <mudulozan@gmail.com>
2018-03-19 18:18:23 +0300

    oldu olacak commiti

---
+++ ProjectImage/src/projectpackage/P4.java
@@ -0,0 +1,123 @@
+package projectpackage;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class P4 extends JFrame{
+	private int width, height, w, m, maxVal, bitsToSkip, bitNum;
+	private int [] pixs, barray;
+	private DrawingPanel dp;
+	
+	P4() {
+		try {
+			FileInputStream fis = new FileInputStream("fool.pbm");
+			BufferedInputStream bis = new BufferedInputStream(fis);
+			byte [] magicNum = new byte [2];
+			bis.read(magicNum);
+			
+			char chByte = (char) bis.read();
+			while(Character.isWhitespace(chByte))
+				chByte = (char) bis.read();
+			
+			byte fb = (byte) chByte;
+			int count = 1;
+			byte [] w = new byte[3];
+			w[0] = fb;
+			
+			byte aByte = (byte) bis.read();
+			while(!Character.isWhitespace(aByte)) {
+				w[count++] = aByte;
+				aByte = (byte) bis.read();
+			}
+			
+			int i = 0;
+			String strWidth = "";
+			while(i < count)
+				strWidth = strWidth + (w[i++]-48);
+			
+			count = 0;
+			byte [] h = new byte[4];
+			aByte = (byte) bis.read();
+			while(!Character.isWhitespace(aByte)) {
+				h[count++] = aByte;
+				aByte = (byte) bis.read();
+			}
+			
+			i = 0;
+			String strHeight = "";
+			while(i < count)
+				strHeight = strHeight + (h[i++]-48);
+			
+			System.out.println(strWidth);
+			System.out.print(strHeight);
+			
+			height = Integer.parseInt(strHeight);
+			width = Integer.parseInt(strWidth);
+			
+			pixs = new int [((width*8)+1)*height];
+			barray = new int [((width*8)+1)*height];
+
+			bitsToSkip = (8-(width % 8));
+			
+			for (int j = 1; j < pixs.length; j++) {
+				pixs[j] = (bis.read());
+			}
+			int c = 0;
+			for(int j = 0; j < width*height; j++) {
+				for (int k = 7; k > 0; k--) {
+					barray[c] = getBit(pixs[j]);
+					c++;
+				}
+			}
+			
+			dp = new DrawingPanel();
+			add(dp);
+		} 
+		catch (FileNotFoundException e) {} 
+		catch (IOException e) { }
+		
+		setSize(width, height);
+		setVisible(true);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+	
+	class DrawingPanel extends JPanel {
+		@Override
+		public void paintComponent(Graphics g) { 
+			super.paintComponent(g);
+			bitNum = 0;
+			for (int row = 0; row < height; row++) { 
+				if (bitNum != 0) 
+					bitNum += bitsToSkip;
+				for (int col = 0; col < width; col++) {
+					g.setColor(getColor(barray[bitNum]));
+					g.fillRect(col, row, 1, 1);
+					bitNum++;
+				}
+			}
+			System.out.println("Done with the painting");
+		}
+	}
+	
+	public Color getColor (int num) {
+		return (num == 1) ? new Color(0, 0, 0): new Color(255, 255, 255);
+	}
+	
+	int getBit(int allBits) {
+		int index = allBits / 8;
+		int bitNum = allBits % 8;
+		int temp = ((pixs[index] >>> (7-bitNum)) & 1);
+		return temp;
+	}
+	
+	public static void main (String[] args) {
+		P4 w = new P4();
+	}	
+}
--- ProjectImage/src/projectpackage/ProjectImage.java
+++ ProjectImage/src/projectpackage/ProjectImage.java
@@ -2,10 +2,13 @@ package projectpackage;
 
 import javax.swing.*;
 
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
 import java.awt.Toolkit;
 import java.awt.event.*;
-import java.io.*;   
+import java.io.*;
+import java.util.Scanner;   
 
 public class ProjectImage extends JFrame {
 	// Menu Items
@@ -16,7 +19,7 @@ public class ProjectImage extends JFrame {
 	private int [] pixs;
 	private int [] barray;
 	private int w,h, mv, maxVal, height, width;
-	//private DrawingPan dp;
+	private DrawingPanel dp;
 	
 	
 	ProjectImage() {
@@ -44,7 +47,7 @@ public class ProjectImage extends JFrame {
 				try {
 		    		// Reading the file's text part
 					fis = new FileInputStream(selectedFile);
-					 byte [] magicNum = new byte[2];
+					byte [] magicNum = new byte[2];
 			    		fis.read(magicNum);
 			    		
 			    		char chByte = (char) fis.read();
@@ -81,6 +84,7 @@ public class ProjectImage extends JFrame {
 			    		String strHeight = "";
 			    		while(i < count)
 			    			strHeight = strHeight + (h[i++]-48);
+			    		System.out.println(strWidth);
 			    		System.out.println(strHeight);
 			    		
 			    		height = Integer.parseInt(strHeight);
@@ -94,6 +98,25 @@ public class ProjectImage extends JFrame {
 			    		
 			    		
 			    	case "P2":
+				    	count = 0;
+						byte [] mv = new byte[3];
+						aByte = (byte) fis.read();
+						while(!Character.isWhitespace(aByte)){
+							mv[count++] = aByte;
+							aByte = (byte) fis.read();
+						}
+						
+						i = 0;
+						String strmaxVal = "";
+						while(i < count)
+							strmaxVal = strmaxVal + (mv[i++]-48);
+						
+						pixs = new int [width*height];
+						pixs[0] = aByte;
+						for (int j = 1; j < height*width; j++) {
+							pixs[j] = (fis.read());
+						}
+						System.out.println("pixs[0]: " + pixs[0]);
 			    		break;
 			    		
 			    		
@@ -105,27 +128,29 @@ public class ProjectImage extends JFrame {
 			    		break;
 			    		
 			    		
-			    	case "P5":
-			    		count = 0;
-			    		byte [] mv = new byte[3];
-			    		aByte = (byte) fis.read();
-			    		while(!Character.isWhitespace(aByte)){
-			    			mv[count++] = aByte;
-			    			aByte = (byte) fis.read();
-			    		}
-			    		
-			    		i = 0;
-			    		String strmaxVal = "";
-			    		while(i < count)
-			    			strmaxVal = strmaxVal + (mv[i++]-48);
-			    		System.out.println(strmaxVal);
+			    	case "P5":		
+			    	count = 0;
+					mv = new byte[3];
+					aByte = (byte) fis.read();
+					while(!Character.isWhitespace(aByte)){
+						mv[count++] = aByte;
+						aByte = (byte) fis.read();
+					}
+					
+					i = 0;
+					strmaxVal = "";
+					while(i < count)
+						strmaxVal = strmaxVal + (mv[i++]-48);
+					System.out.println(strmaxVal);
 
-			    		pixs = new int [width*height];
-			    		pixs[0] = aByte;
-			    		for (int j = 1; j < height*width; j++) {
-			    			pixs[j] = (fis.read());
-			    		}
-			    		break;
+					pixs = new int [width*height];
+					pixs[0] = aByte;
+					for (int j = 1; j < height*width; j++) {
+						pixs[j] = (fis.read());
+						System.out.println("read");
+					}
+					System.out.println("test");
+					break;
 			    		
 			    		
 			    	case "P6":
@@ -134,15 +159,16 @@ public class ProjectImage extends JFrame {
 			    	    JOptionPane.showMessageDialog(menuOpen, "Only PGM,PBM and PPM files are allowed.", "Error", JOptionPane.ERROR_MESSAGE);
 			    		break;
 			    	}
+			    	
 				}  
-				/*
-				dp = new DrawingPan();
-				add(dp);
-				*/
+				
 				catch(FileNotFoundException e1){
 					System.out.println(e1);
 				}
 				catch(IOException e2){}
+				dp = new DrawingPanel();
+				add(dp);
+				System.out.println("reach");
 	    	}
 	    });
 	    
@@ -162,7 +188,7 @@ public class ProjectImage extends JFrame {
 	
 		// Menu bar
 	    menubar.add(file);
-	
+	    
 	    // Window
 	    Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
 	    setJMenuBar(menubar);
@@ -170,8 +196,23 @@ public class ProjectImage extends JFrame {
 		setSize(dim.width,dim.height);
 		setVisible(true);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
+	}
 
+	class DrawingPanel extends JPanel {
+		@Override
+		public void paintComponent(Graphics g) {
+			System.out.println("I am here");
+			super.paintComponent(g);
+			for(int row = 0; row < height; row++) {
+				for(int col = 0; col < width; col++) {
+					
+					Color c = new Color(pixs[row*width+col],pixs[row*width+col],pixs[row*width+col]);
+					g.setColor(c);
+					g.fillRect(col, row, 1, 1);
+				}
+			}
+			System.out.println("Now here");
+		}
 	}
 
 	
